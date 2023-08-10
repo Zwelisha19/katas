1de4951fba2a6884043afcce3426137680724b18
Zwelisha19 <106339434+Zwelisha19@users.noreply.github.com>
2023-08-10 10:43:33 +0200

    Create pin_validate.js

---
+++ problems/pin_validate.js
@@ -0,0 +1,8 @@
+const pin =prompt('Enter the pin');
+
+   if (pin.length===4 ||         pin===Number){
+    console.log("Pin accepted");
+   }
+   else{
+    console.log("Invalid Pin!!!!!!!!!");
+   }
